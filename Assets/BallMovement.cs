8c13b9c2d12c2b33a6d225ec33dedc29f95a50d7
Max Shalom <max_shalom121@flatbush.org>
2017-09-20 16:42:49 -0400

    Add files via upload
    Main (2nd) Upload
    v1.0 - Assets (Scripts, Scenes, Art)

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+    Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(speed, 0, 0));
+    }
+}
